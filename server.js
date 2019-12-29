f542c6ae9e17ec18e8a8e0ba7d176adf3d70b205
Itai Naor <itain@realcommerce.co.il>
2019-12-29 02:57:10 +0200

    fix heroku 8

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/Itai-Naor-28-12-2019'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+  res.sendFile(path.join(__dirname+'/Itai-Naor-28-12-2019/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
